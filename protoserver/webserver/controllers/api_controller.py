717e02849149245aee2b591d349154219cb3d801
Enrico Carniani <>
2023-04-14 15:55:44 +0200

    adding service for conversion

--- protoserver/webserver/controllers/api_controller.py
+++ protoserver/webserver/controllers/api_controller.py
@@ -5,6 +5,7 @@ from flask.helpers import request
 from webserver import controller
 import json
 import os
+import shutil
 
 from jobs import analyse, convert, submit
 
@@ -14,28 +15,61 @@ class ApiController(controller.Controller):
 		super().__init__(app, socketio)
 
 		@app.route('/resources', methods = ['POST'])
-		def resources():
+		def create_resource():
 			tbx = request.json.get('tbx')
 			operation = analyse.Analyse(tbx)
 			resource_string = operation.execute()
 			resource = json.loads(resource_string)
-			self.save_resource(tbx, resource['id'], 'tbx')
-			self.save_resource(resource['sparql'], resource['id'], 'sparql')
-			self.save_resource(json.dumps(resource['metadata']), resource['id'], 'metadata')
 
-			return Response(resource_string, mimetype='application/json')
+			del resource['sparql']	# FIXME: should not be produced
 
-		@app.route('/resources/<id>/submit', methods = ['POST'])
-		def resources_id(id: str):
+			self.delete_resource(resource['id'])
+			self.save_resource_property(tbx, resource['id'], 'tbx')
+			self.save_resource_property(json.dumps(resource['metadata']), resource['id'], 'metadata')
+
+			return Response(json.dumps(resource), mimetype='application/json')
+
+		@app.route('/resources/<resource_id>/sparql', methods = ['GET'])
+		def get_resource_sparql(resource_id: str):
+			content = self.read_resource_property(resource_id, 'sparql')
+			if content is None:
+				content = self.create_resource_sparql(resource_id)
+			return Response(content, mimetype='application/sparql-query')
+
+		@app.route('/resources/<resource_id>/submit', methods = ['POST'])
+		def submit_resource_id(resource_id: str):
+			filename = self.get_resource_path(resource_id, 'sparql')
+			if not os.path.exists(filename):
+				self.create_resource_sparql(resource_id)
 			repository = request.json.get('repository')
-			infilename = os.path.join(self.app.config['RESOURCES_FOLDER'], id, 'sparql')
-			operation = submit.Submit(infilename, repository)
+			operation = submit.Submit(filename, repository)
 			operation.execute()
-			return Response(json.dumps({"repository": repository}), mimetype='application/json')
+			return Response(json.dumps({}), mimetype='application/json')
 
-	def save_resource(self, content: str, resource_id: str, property: str):
-		dirname = os.path.join(self.app.config['RESOURCES_FOLDER'], resource_id)
+	def delete_resource(self, resource_id):
+		dirname = self.get_resource_path(resource_id)
+		shutil.rmtree(dirname, ignore_errors=True)
+
+	def save_resource_property(self, content: str, resource_id: str, property: str):
+		dirname = self.get_resource_path(resource_id)
 		filename = os.path.join(dirname, property)
 		os.makedirs(dirname, exist_ok=True)
 		with open(filename, mode='w') as file:
 			file.write(content)
+
+	def read_resource_property(self, resource_id: str, property: str) -> str:
+		filename = self.get_resource_path(resource_id, property)
+		if os.path.exists(filename):
+			with open(filename) as file:
+				return file.read()
+			
+	def create_resource_sparql(self, resource_id):
+		infilename = self.get_resource_path(resource_id, 'tbx')
+		outfilename = self.get_resource_path(resource_id, 'sparql')
+		operation = convert.Convert(infilename, outfilename)
+		operation.execute()
+		content = self.read_resource_property(resource_id, 'sparql')
+		return content
+
+	def get_resource_path(self, resource_id: str, *args) -> str:
+		return os.path.join(self.app.config['RESOURCES_FOLDER'], resource_id, *args)
